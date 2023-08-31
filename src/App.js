505a7125c87ece1c0062ec97c26e6275117b3923
5r1ram <sriramiiit33@gmail.com>
2023-08-31 22:42:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com?apikey=ce86583b";
+
+const movieOne = {
+  Title: "Iron Man",
+  Year: "2008",
+  imdbID: "tt0371746",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    // const API_KEY = process.env.REACT_APP_API_URL;
+    // const response = await fetch(
+    //   `http://www.omdbapi.com?apikey=${API_KEY}&s=${title}`
+    // );
+
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Iron Man");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Movix</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found!</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
